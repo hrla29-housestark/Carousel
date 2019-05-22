b7ddb0be4dbc4fb9f31e2b2227177b38cc43ae2e
Wendy Huynh <wendyhuynhs@gmail.com>
2019-05-21 22:17:48 -0700

    Working on Popup Modal [Size Chart]

--- client/src/components/SizeChart.jsx
+++ client/src/components/SizeChart.jsx
@@ -1,11 +1,60 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../assets/css/sizeChart.css';
 
 const SizeChart = props => (
-  <div>
-    <svg class="gl-icon back-icon___9qTR7">
-      <use xlink:href="#arrow-back" />
-    </svg>
+  <div className={styles.sizeChartBody}>
+    <h5> Size Chart</h5>
+    <div>
+      <section>
+        <header>
+          <h4>Men's and women's adidas footwear sizing</h4>
+          <h5>FIND YOUR SIZE</h5>
+          <a>Do you know how to measure?</a>
+        </header>
+        <div className={styles.columnsTable}>
+          <table className={styles.table}>
+            <tbody>
+              <tr>
+                <td className={[styles.td, styles.tdBorder].join(' ')}>
+                  <span>Heel-toe measurement</span>
+                </td>
+              </tr>
+              <tr>
+                <td className={[styles.td, styles.tdBorder].join(' ')}>
+                  <span>US - Men's</span>
+                </td>
+              </tr>
+              <tr>
+                <td className={[styles.td, styles.tdBorder].join(' ')}>
+                  <span>US - Women's</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td className={[styles.td, styles.tdBorder].join(' ')}>
+                  <span>UK</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td className={[styles.td, styles.tdBorder].join(' ')}>
+                  <span>EU</span>
+                </td>
+              </tr>
+              <tr>
+                <td className={[styles.td, styles.tdBorder].join(' ')}>
+                  <span>JP</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </div>
   </div>
 );
 
 export default SizeChart;
+
+// <button> x </button>
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,18 +2,31 @@ import React from 'react';
 import styles from '../assets/css/styles.css';
 import Checkout from './checkout.jsx';
 import TopNavigation from './TopNavigation.jsx';
+import SizeChart from './SizeChart.jsx';
+import sizeChartStyles from '../assets/css/sizeChart.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isOpen: false
+    };
+    this.renderModal = this.renderModal.bind(this);
+  }
+  renderModal() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
   render() {
     return (
       <div>
         <TopNavigation />
+        <div className={sizeChartStyles.body}>
+          {this.state.isOpen ? <SizeChart /> : null}
+        </div>
         <div className={styles.checkoutPlacement}>
-          <Checkout />
+          <Checkout isOpen={this.renderModal} render={this.state.isOpen} />
         </div>
       </div>
     );
