234a4030b279b0dd28d386f43d16eae2a3bf9289
Wendy Huynh <wendyhuynhs@gmail.com>
2019-05-22 18:49:47 -0700

    worked on checkout component

--- client/src/components/Selection.jsx
+++ client/src/components/Selection.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import styles from '../assets/css/styles.css';
 
 const Selection = props => (
   <div>
-    <div> Shoe Size</div>
-    <div> Quantity</div>
+    <div className={[styles.row, styles.size_quantity_container].join(' ')} />
+    <div className={styles.dropdown}>
+      <button className={styles.dropdownSelect}>
+        <span> Select Size</span>
+        <svg
+          class="gl-icon gl-dropdown__select-icon"
+          data-di-rand="1558570244522"
+        />
+      </button>
+    </div>
   </div>
 );
 
