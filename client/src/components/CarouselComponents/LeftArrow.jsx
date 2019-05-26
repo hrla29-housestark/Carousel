d1ae61304add71e22eb3c8346cc23e8622db5be4
Wendy Huynh <wendyhuynhs@gmail.com>
2019-05-26 12:02:46 -0700

    Carousel Components Folder

---
+++ client/src/components/CarouselComponents/LeftArrow.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from '../../assets/css/carousel.css';
+
+const LeftArrow = props => (
+  <div>
+    <svg
+      className={styles.backArrowSvg}
+      viewBox="0 0 24 24"
+      width="50%"
+      height="50%"
+    >
+      <path
+        d="M17.59 7l5 5-5 5M0 12h22"
+        fill="none"
+        stroke="currentColor"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+      />
+    </svg>
+  </div>
+);
+
+export default LeftArrow;
---
+++ client/src/components/CarouselComponents/SlideImage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from '../../assets/css/carousel.css';
+
+const SlideImage = props => <div className={styles.slideImage}>T</div>;
+
+export default SlideImage;
